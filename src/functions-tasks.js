19a1085cbd8a1e3dcb1b5d9621701d9d9e236f69
koonukaame <marialezhebokova@yandex.ru>
2025-04-14 18:55:07 +0300

    feat: solve getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -78,8 +78,10 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (num) => {
+    return num ** exponent;
+  };
 }
 
 /**
