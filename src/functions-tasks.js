b2fe482678e0b0977de5bf663d5ad2aa382a4d4f
koonukaame <marialezhebokova@yandex.ru>
2025-04-14 19:08:13 +0300

    feat: solve retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -144,8 +144,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    try {
+      return func();
+    } catch (error) {
+      if (attempts <= 1) {
+        throw error;
+      }
+    }
+    return retry(func, attempts - 1)();
+  };
 }
 
 /**
